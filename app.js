29aa399d9f84585441947458ca98afe8ff5bae10
Randy Harkediansa <randy.harkediansa@gmail.com>
2019-06-03 17:30:45 +0800

    Playing with chalk

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // const fs = require('fs');
-
 const utils = require('./utils.js');
 const getNotes = require('./notes.js');
+
+const chalk = require('chalk');
 
 // fs.writeFileSync('notes.txt', 'This file was edited by Nodejs.');
 // fs.appendFileSync('notes.txt', ' This text is appended by Nodejs')
@@ -11,3 +12,8 @@ console.log(testSum);
 
 const notes = getNotes()
 console.log(notes);
+
+
+console.log(chalk.green.inverse('Success!!'));
+
+console.log(process.argv)
